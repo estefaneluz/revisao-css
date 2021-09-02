ddb21143e11b5fdd1d4c9534c9ba58d3ee11d22a
Estefane <estefanecortes@gmail.com>
2021-09-02 19:53:50 -0300

    criando componente Modal

---
+++ classe-01/src/components/Modal/index.js
@@ -0,0 +1,29 @@
+import './styles.css'
+import emojiFeliz from '../../assets/emoji-feliz.svg'
+import emojiNeutro from '../../assets/emoji-neutro.svg'
+import emojiTriste from '../../assets/emoji-triste.svg'
+import fechar from '../../assets/botao-fechar.svg'
+
+export default function Modal() {
+    return (
+        <div className="card">
+            <img src={fechar} alt="icone de fechar"/>
+            <h3>How satisfied are you with our customer support performance?</h3>
+            <div>
+                <div>
+                    <img src={emojiTriste} alt="rosto triste"/>
+                    <p>Unhappy</p>
+                </div>
+                <div>
+                    <img src={emojiNeutro} alt="rosto Neutro"/>
+                    <p>Neutral</p>
+                </div>
+                <div>
+                    <img src={emojiFeliz} alt="rosto feliz"/>
+                    <p>Satisfied</p>
+                </div>
+            </div>
+            <button>Close</button>
+        </div>
+    )
+}
