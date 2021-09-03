8fb7fdda4e6f07a8dedcc73a4b8b9005c46a26de
Estefane <estefanecortes@gmail.com>
2021-09-02 21:42:44 -0300

    criação do array de cards e ajustes no css

--- classe-02/src/App.js
+++ classe-02/src/App.js
@@ -1,16 +1,40 @@
 import './App.css';
 import Card from './components/Card'
 import search from './assets/icone-lupa.svg'
+import globo from './assets/icone-globo.svg'
+import mapa from './assets/icone-mapa.svg'
+
 
 function App() {
+  const cards = [
+    {
+      titulo: "Search", 
+      descricao: "Search in any topic you are interested and find any answers you need.",
+      imagem: search
+    },
+    {
+      titulo: "Browser", 
+      descricao: "Search in any topic you are interested and find any answers you need.",
+      imagem: globo
+    },
+    {
+      titulo: "Maps", 
+      descricao: "Search in any topic you are interested and find any answers you need.",
+      imagem: mapa
+    }
+  ]
   return (
     <div className="container">
       <h1>Browse collections</h1>
-      <Card
-        titulo="Search"
-        descricao="Search in any topic you are interested and find any answers you need."
-        img={search}
-      />
+      <div className="row">
+        {cards.map( card =>
+        <Card
+          titulo={card.titulo}
+          descricao={card.descricao}
+          img={card.imagem}
+        />
+        )}
+      </div>
     </div>
   );
 }
