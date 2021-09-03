6ff90a67fafcee8604bd605e95e00a11b99d7d40
Estefane <estefanecortes@gmail.com>
2021-09-02 22:46:04 -0300

    estilo base do componente ContactIndividual

--- classe-03/src/components/ContactIndividual/index.js
+++ classe-03/src/components/ContactIndividual/index.js
@@ -3,13 +3,16 @@ import chatIcon from '../../assets/icone-chat.svg'
 
 export default function ContactIndividual({img, nome, local}) {
     return(
-        <div>
-            <img src={img} alt={`Foto de perfil de ${nome}`}/>
+        <div className="contact-card">
+            <img 
+                className="contact-img"
+                src={img} alt={`Foto de perfil de ${nome}`}
+            />
             <div>
                 <h3>{nome}</h3>
                 <p>{local}</p>
             </div>
-            <img src={chatIcon} alt="icone de chat"/>
+            <img id="chat-icon" src={chatIcon} alt="icone de chat"/>
         </div>
     );
 }
