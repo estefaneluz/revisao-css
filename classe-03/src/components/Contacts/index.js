3a13a08e029ac26bbdbcf35a895bf7ed5141dd1d
Estefane <estefanecortes@gmail.com>
2021-09-02 23:00:00 -0300

    componente Contacts ok

--- classe-03/src/components/Contacts/index.js
+++ classe-03/src/components/Contacts/index.js
@@ -1,6 +1,9 @@
 import './styles.css'
 import ContactIndividual from '../ContactIndividual'
 import lise from '../../assets/lise.png'
+import emily from '../../assets/emily.png'
+import jake from '../../assets/jake.png'
+import cheryl from '../../assets/cheryl.png'
 
 export default function Contacts(){
     const contacts = [
@@ -8,9 +11,24 @@ export default function Contacts(){
             nome: "Lise",
             imagem: lise,
             local: "Bergen, Norway"
-        }
+        },
+        {
+            nome: "Emily",
+            imagem: emily,
+            local: "Bergen, Norway",
+        },
+        {
+            nome: "Jake",
+            imagem: jake,
+            local: "Bergen, Norway",
+        },
+        {
+            nome: "Cheryl",
+            imagem: cheryl,
+            local: "Bergen, Norway",
+        },
     ]
-    
+
     return(
         <div className="card-contacs">
             <h2>Contacs <span>43</span></h2>
