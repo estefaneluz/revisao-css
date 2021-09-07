96f549e4b5ab74fc50b378a6a84ca47ae52742e2
estefaneluz <estefanecortes@gmail.com>
2021-09-06 23:05:42 -0300

    componente LargeCard criado

---
+++ classe-04/src/components/LargeCard/index.js
@@ -0,0 +1,24 @@
+import './styles.css'
+
+export default function LargeCard() {
+    return(
+        <div className="large-card">
+            <div>
+                <img/>
+                <div>
+                    <h2>Usuário</h2>
+                    <p>UX Designer</p>
+                    <button>
+                        <img/> 
+                        Seguindo
+                    </button>
+                </div>
+            </div>
+            <div>
+                <img/>
+                <img/>
+                <img/>
+            </div>
+        </div>
+    )
+}
