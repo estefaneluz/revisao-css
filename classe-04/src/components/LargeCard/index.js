dea0c952f03a422c4dd1f605150f64973a377086
estefaneluz <estefanecortes@gmail.com>
2021-09-07 00:00:59 -0300

    componente LargeCard estilizado

--- classe-04/src/components/LargeCard/index.js
+++ classe-04/src/components/LargeCard/index.js
@@ -1,23 +1,30 @@
 import './styles.css'
 
+import userImg from '../../assets/jenny.png'
+import design04 from '../../assets/design4.png'
+import design05 from '../../assets/design5.png'
+import design06 from '../../assets/design6.png'
+import followImg from '../../assets/icone-usuario-dark.svg'
+
+
 export default function LargeCard() {
     return(
         <div className="card large-card">
-            <div>
-                <img/>
+            <div className="user-info">
+                <img className="user-img" src={userImg}/>
                 <div>
                     <h2>Usuário</h2>
                     <p>UX Designer</p>
-                    <button>
-                        <img/> 
+                    <button className="btn-white">
+                        <img src={followImg}/> 
                         Seguindo
                     </button>
                 </div>
             </div>
-            <div>
-                <img/>
-                <img/>
-                <img/>
+            <div className="design-container">
+                <img src={design04}/>
+                <img src={design05}/>
+                <img src={design06}/>
             </div>
         </div>
     )
