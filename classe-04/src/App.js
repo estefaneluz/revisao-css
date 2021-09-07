9b99e4daff02687e350953f4f0484c7261ec564e
estefaneluz <estefanecortes@gmail.com>
2021-09-06 23:30:40 -0300

    componente SmallCard estilizado

--- classe-04/src/App.js
+++ classe-04/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import SmallCard from './components/SmallCard';
+import LargeCard from './components/LargeCard';
 
 function App() {
   return (
     <>
       <SmallCard/>
+      <LargeCard/>
     </>
   );
 }
--- classe-04/src/components/SmallCard/index.js
+++ classe-04/src/components/SmallCard/index.js
@@ -1,20 +1,28 @@
 import './styles.css'
+import userImg from '../../assets/jenny.png'
+import design01 from '../../assets/design1.png'
+import design02 from '../../assets/design2.png'
+import design03 from '../../assets/design3.png'
+import followImg from '../../assets/icone-usuario.svg'
 
 export default function SmallCard() {
     return(
-        <div className='small-card'>
-            <div>
-                <img/>
+        <div className='card small-card'>
+            <div className="user-info">
+                <img className="user-img" src={userImg}/>
                 <div>
                     <h2>Jenny Molla</h2>
                     <p>UX Designer</p>
                 </div>
-                <button><img/>Seguir</button>
+                <button className="btn-dark">
+                    <img src={followImg}/>
+                    Seguir
+                </button>
             </div>
-            <div>
-                <img/>
-                <img/>
-                <img/>
+            <div className='design-container'>
+                <img src={design01}/>
+                <img src={design02}/>
+                <img src={design03}/>
             </div>
         </div>
     )
