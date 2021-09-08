f204e510c4949412b318be57639b3b418a4054df
estefaneluz <estefanecortes@gmail.com>
2021-09-08 11:44:38 -0300

    componente header criado

--- casa-05/src/components/Header/index.js
+++ casa-05/src/components/Header/index.js
@@ -1,7 +1,12 @@
+import './styles.css'
 import Navbar from "../Navbar";
+import background from '../../assets/image-header.jpg'
 
 export default function Header() {
     return (
-        <Navbar/>
+        <div className="container-header">
+            <Navbar/>
+            <h2>We are creatives</h2>
+        </div>
     )
 }
