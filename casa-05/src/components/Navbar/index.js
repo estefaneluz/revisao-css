db7d8bd56dff113a8435b3bfc93ad629f06375e8
estefaneluz <estefanecortes@gmail.com>
2021-09-08 10:59:09 -0300

    componente Navbar criado

---
+++ casa-05/src/components/Navbar/index.js
@@ -0,0 +1,17 @@
+import './styles.css'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <h1>Sunnyside</h1>
+            <ul>
+                <li>About</li>
+                <li>Services</li>
+                <li>Project </li>
+                <li>
+                    <button>Contact</button>
+                </li>
+            </ul>
+        </nav>
+    )
+}
