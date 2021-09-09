1e03d8392392cc523f49c5f3eaf09d913ea57f9f
estefaneluz <estefanecortes@gmail.com>
2021-09-09 19:57:50 -0300

    ajustes

--- casa-05/src/components/Header/index.js
+++ casa-05/src/components/Header/index.js
@@ -1,11 +1,12 @@
-import './styles.css'
+import "./styles.css";
 import Navbar from "../Navbar";
 
 export default function Header() {
-    return (
-        <div className="container-header">
-            <Navbar/>
-            <h2>We are creatives</h2>
-        </div>
-    )
+  return (
+    <div className="header-wrapper">
+      <Navbar />
+      <h2>We are creatives</h2>
+      <div className="arrow" />
+    </div>
+  );
 }
--- casa-05/src/components/LearnMoreSection/index.js
+++ casa-05/src/components/LearnMoreSection/index.js
@@ -1,16 +1,16 @@
-import './styles.css'
+import "./styles.css";
 
-export default function LearnMoreSection({title, description, img, classe}) {
-    return (
-        <div className={`${classe ? classe : ''} learn-more-container`}>
-            <div className="content">
-                <h3>{title}</h3>
-                <p>{description}</p>
-                <a href="/">Learn More</a>
-            </div>
-            <div className="half-img">
-                <img src={img} alt={`Ilustração de ${title}`}/>
-            </div>
-        </div>
-    )
+export default function LearnMoreSection({ title, description, img, classe }) {
+  return (
+    <div className={`${classe ? classe : ""} learn-more-container`}>
+      <div className="content">
+        <h3>{title}</h3>
+        <p>{description}</p>
+        <a href="/">Learn More</a>
+      </div>
+      <div className="half-img">
+        <img src={img} alt={`Ilustração de ${title}`} />
+      </div>
+    </div>
+  );
 }
